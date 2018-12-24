b82c1d542e5c6df891f467772d8b7df7f40c56da
ArkabaevDima <43188881+ArkabaevDima@users.noreply.github.com>
2018-12-25 00:09:54 +0300

    pract_2

---
+++ TestAuthor.java
@@ -0,0 +1,9 @@
+package ru.mirea.arkabaev.pract2;
+import java.lang.String;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Sue Grand", "suegrand@mail.com", 'M');
+        System.out.println(a1.toString());
+    }
+}
